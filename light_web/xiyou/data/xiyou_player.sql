0a860fc9a87936898d4cfccf724ed3646118a516
liufengxu <onx0320@126.com>
2020-07-12 23:53:11 +0800

    add db data

---
+++ light_web/xiyou/data/xiyou_player.sql
@@ -0,0 +1,12 @@
+INSERT INTO light_web.xiyou_player (id,name,party,score) VALUES 
+(1,'TangSanzang','Fo',5)
+,(2,'SunWukong','Fo',95)
+,(3,'ZhuBajie','Fo',70)
+,(4,'ShaSeng','Fo',65)
+,(5,'BaiLongma','Fo',60)
+,(6,'BaiguJing','Yao',63)
+,(7,'ZhizhuJing','Yao',55)
+,(8,'NiuMowang','Yao',90)
+,(9,'TieshanGongzhu','Yao',66)
+,(10,'ErlangShen','Shen',96)
+;
