b6f3d56f607992c3708bfb1a4a5daafd59fcd147
liufengxu <onx0320@126.com>
2020-07-15 00:12:36 +0800

    xgb classify

--- learn_sk/learn_xgb.py
+++ learn_sk/learn_xgb.py
@@ -1,13 +1,57 @@
-import pandas as pd
+from sklearn.datasets import load_iris
+import xgboost as xgb
+from xgboost import plot_importance
+from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import Imputer
 
-data = pd.read_csv('../input/train.csv')
-data.dropna(axis=0, subset=['SalePrice'], inplace=True)
-y = data.SalePrice
-X = data.drop(['SalePrice'], axis=1).select_dtypes(exclude=['object'])
-train_X, test_X, train_y, test_y = train_test_split(X.as_matrix(), y.as_matrix(), test_size=0.25)
+# read in the iris data
+iris = load_iris()
 
-my_imputer = Imputer()
-train_X = my_imputer.fit_transform(train_X)
-test_X = my_imputer.transform(test_X)
+X = iris.data
+y = iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234565)
+
+params = {
+    'booster': 'gbtree',
+    'objective': 'multi:softmax',
+    'num_class': 3,
+    'gamma': 0.1,
+    'max_depth': 6,
+    'lambda': 2,
+    'subsample': 0.7,
+    'colsample_bytree': 0.7,
+    'min_child_weight': 3,
+    'silent': 1,
+    'eta': 0.1,
+    'seed': 1000,
+    'nthread': 4,
+}
+
+plst = params.items()
+
+
+dtrain = xgb.DMatrix(X_train, y_train)
+num_rounds = 500
+model = xgb.train(plst, dtrain, num_rounds)
+
+# 对测试集进行预测
+dtest = xgb.DMatrix(X_test)
+ans = model.predict(dtest)
+
+# 计算准确率
+def cal_accuracy(ans, y_test):
+    cnt1 = 0
+    cnt2 = 0
+    for i in range(len(y_test)):
+        if ans[i] == y_test[i]:
+            cnt1 += 1
+        else:
+            cnt2 += 1
+    return cnt1 / (cnt1 + cnt2) 
+
+print("Accuracy: %.2f %% " % (100 * cal_accuracy(ans, y_test))
+
+# 显示重要特征
+plot_importance(model)
+plt.show()
