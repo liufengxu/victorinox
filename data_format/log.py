8f1cd45f7ac39cb4c1c9e45c6d84678ec8a92033
liufengxu <onx0320@126.com>
2020-07-20 00:31:41 +0800

    add explain

--- data_format/log.py
+++ data_format/log.py
@@ -15,11 +15,11 @@ LOGGER = logging.getLogger('default')
 
 def init_logger(name=None):
     pattern = "logs/{0}.log"
-    default_name = "exposure_{0}".format(os.getpid())
+    default_name = "xxxxxx_{0}".format(os.getpid())
     name = default_name if not name else name
     logFormater = logging.Formatter("%(asctime)s.%(msecs)03d %(filename)s[line:%(lineno)d] %(levelname)s %(message)s",
                                     datefmt='%Y-%m-%d %H:%M:%S')
-    fileHandler = TimedRotatingFileHandler(pattern.format(name), when="D", backupCount=10)
+    fileHandler = TimedRotatingFileHandler(pattern.format(name), when="D", backupCount=10)  # 按天切分
     fileHandler.setFormatter(logFormater)
     LOGGER.addHandler(fileHandler)
 
