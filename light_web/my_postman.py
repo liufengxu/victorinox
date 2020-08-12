4f8e4387dd620dd556c1280266e098690c13af2f
liufengxu <onx0320@126.com>
2020-08-12 23:58:39 +0800

    add postman

---
+++ light_web/my_postman.py
@@ -0,0 +1,34 @@
+import json
+import requests
+
+class PostMan(object):
+    def set_headers(headers):
+        self.headers = headers
+
+    def set_type(t):
+        pass
+
+
+
+
+my_url = 'http://offline_website/compare'
+my_url_get = 'http://offline_website/alive'
+d = {'guid': '865555555555', 'search_id': '29111111111111'}
+
+
+def simulate_post(url, para):
+    data = json.dumps(para)
+    response = requests.post(url=url, data=data, timeout=40, headers={'content-type': 'application/json'})
+    return response.status_code, response.text
+
+
+def simulate_get(url):
+    response = requests.get(url=url, timeout=40)
+    return response.status_code, response.text
+
+
+code, text = simulate_post(my_url, d)
+code, text = simulate_get(my_url_get)
+print(code)
+print(text)
+
